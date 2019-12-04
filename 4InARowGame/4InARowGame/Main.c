411bb166b6bdec27f31a214111ef0275bf78c4d7
Tamir <1tamir198@gmail.com>
2019-12-04 22:47:51 +0200

    Adding first main

---
+++ 4InARowGame/4InARowGame/Main.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "stdio.h"
+#include "stdlib.h"
+
+/*************** Board ***************/
+#define ROWS 6
+#define COLS 7
+char board[ROWS][COLS];
+
+
+/*********** Declarations ************/
+
+/// This function initializes the game board by assigning each cell
+/// with ' ' (resulting with an empty game board).
+void initBoard();
+
+/// This function gets a row number and a column number (a cell),
+/// and returns the character in that cell (could be 'X', 'O' or ' ').
+/// For example:
+/// char c = getCell(1, 1);
+char getCell(int row, int col);
+
+/// This function gets a row number, a column number and a sign,
+/// and assigns the cell with the given sign.
+/// For example:
+/// setCell(1, 1, 'X');
+void setCell(int row, int col, char sign);
+
+/// This function clears the screen.
+void clearScreen();
+
+
+/*************** Main ****************/
+void main(){
+
+}
+
+
+/********** Implementations **********/
+char getCell(int row, int col){
+	return board[row - 1][col - 1];
+}
+
+
+void setCell(int row, int col, char sign){
+	board[row - 1][col - 1] = sign;
+}
+
+void initBoard(){
+	int i, j;
+
+	for (i = 0; i < ROWS; i++){
+		for (j = 0; j < COLS; j++){
+			setCell(i + 1, j + 1, ' ');
+		}
+	}
+}
+
+void clearScreen(){
+	system("cls");
+}
